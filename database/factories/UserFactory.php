4a85f9da85a07f4334d61fd42f1b420a084e68d3
Frans <frans_421_12@hotmail.com>
2026-04-10 23:19:25 -0500

    feat: add uuid in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'role' => 'student',
+            'uuid' => (string) Str::uuid(),
             'remember_token' => Str::random(10),
         ];
     }
